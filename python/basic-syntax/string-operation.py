292d8bdd3b931bf05d87598c50e3fa5f95f12ab4
sheep9275 <sheep9275@gmail.com>
2013-09-25 00:54:18 +0800

    Tried regex with unicode character to format file

--- python/basic-syntax/string-operation.py
+++ python/basic-syntax/string-operation.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python
 # -*- utf-8 -*-
+#======================================================================
 
-# substring operation
-my_str = 'somestring'
-char = my_str[0]
-char = my_str[-1]
-sub_str = my_str[1:3]
+import os, re
 
-# string pattern operation
-S.find('pa')
-S.replace('pa', "XYZ")
+#======================================================================
 
-line = 'aaa,bbb,cccc,dd'
-line.split(',')
+DIR_NAME   = os.path.dirname(__file__)
+UTF_SAMPLE = os.path.join(DIR_NAME, 'city-data-1.txt')
 
+#======================================================================
+
+def substring_operations():
+    my_str = 'somestring'
+    char = my_str[0]
+    char = my_str[-1]
+    sub_str = my_str[1:3]
+
+def str_pattern():    
+    S.find('pa')
+    S.replace('pa', "XYZ")
+    line = 'aaa,bbb,cccc,dd'
+    line.split(',')
+
+def utf8_str():
+    record = {}
+    with open(UTF_SAMPLE, 'r') as f:
+        for line in f.readlines():
+            region, city, latitude, longitude = line.split()
+            latitude  = float((re.search('^北纬(?P<latitude>.*)$' , latitude )).group('latitude'))
+            longitude = float((re.search('^东经(?P<longitude>.*)$', longitude)).group('longitude'))
+            if region not in record:
+                record[region] = []
+            record[region].append((city, latitude, longitude))
+    for region in record:
+        print(region)
+        print(record[region])
+
+if __name__ == '__main__':
+    utf8_str()
